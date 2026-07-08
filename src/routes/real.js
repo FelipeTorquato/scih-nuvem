636ca4257183c541c93cc7aef8586a83eefb738a
FELIPE CAMURÇA TORQUATO <felipe.torquato@tce.ce.gov.br>
2026-07-08 11:20:17 -0300

    atualizando arquivo laudo de exemplo

--- src/routes/real.js
+++ src/routes/real.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/laudos/:prontuario', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../r.e.a.l_exemplo.pdf'));
+    res.sendFile(path.join(__dirname, '../../r.e.a.l_exemplo-1.pdf'));
 });
 
 router.get('/laudos', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../r.e.a.l_exemplo.pdf'));
+    res.sendFile(path.join(__dirname, '../../r.e.a.l_exemplo-1.pdf'));
 });
 
 module.exports = router;
